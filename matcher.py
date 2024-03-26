d8abaaeae6ee1af520db5a1646524728922a9226
cblystone67 <christopherblystone1@gmail.com>
2024-03-26 05:40:35 -0700

    completed the prefix file and added the matcher file.

---
+++ matcher.py
@@ -0,0 +1,12 @@
+from Queue import Queue
+from stack import Stack
+
+
+def main():
+
+    print("End of main")
+
+
+# Don't run main on import
+if __name__ == "__main__":
+    main()
--- postfix_2_infix.py
+++ postfix_2_infix.py
@@ -2,8 +2,25 @@ from Queue import Queue
 from stack import Stack
 
 
-def main():
+def postfix_2_infix(queue):
+    s = Stack([])
+    while not queue.is_empty():
+        c = queue.deq()
+        if c in "+-/*":
+            op2 = s.pop()
+            op1 = s.pop()
+            s.push(op1 + c + op2)
+        else:
+            s.push(c)
 
+    return s.pop()
+
+
+def main():
+    queue = Queue(['1', '2', '+'])
+    queue2 = Queue(['5', '4', '*', '7', '+'])
+    queue3 = Queue(['1', '2', '+'])
+    print(postfix_2_infix(queue))
     print("End of Main")
 
 
