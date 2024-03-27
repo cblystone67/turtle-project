594cb36397fce458cbdc860d4c5fd732b54c9665
cblystone67 <christopherblystone1@gmail.com>
2024-03-27 12:20:36 -0700

    Working through problem 2 problems

--- matcher.py
+++ matcher.py
@@ -2,8 +2,30 @@ from Queue import Queue
 from stack import Stack
 
 
-def main():
+def match(string):
+    braces_dict = {'(': ')', '[': ']', '{': '}'}
+    stack = Stack([])
+    for char in string:
+        if char in braces_dict:
+            stack.push(char)
+        elif char in braces_dict.values():
+            if not stack or braces_dict[stack.pop()] != char:
+                return False
+    return stack.size() == 0
 
+
+def main():
+    str1 = "([])"
+    str2 = "[(]}"
+    str3 = "(hello"
+    braces_dict = {'(': ')', '[': ']', '{': '}'}
+    stack = Stack([])
+    for char in str1:
+        print(char)
+        if char in braces_dict:
+            stack.push(char)
+            print("stack", stack)
+    print(match(str3))
     print("End of main")
 
 
