f78bffcf99edb0198576c8dd7cc0da72de18925e
cblystone67 <christopherblystone1@gmail.com>
2024-04-07 21:02:48 -0700

    working on binary generator

--- binary.py
+++ binary.py
@@ -5,13 +5,18 @@ from stack import Stack
 def generate_binary_numbers(N):
     numbers = Queue([])
     stack = Stack()
-
-    return numbers
+    if N >= 1:
+        generate_binary_numbers(N // 2)
+        # numbers = N % 2
+    print(N % 2, end='')
+    # return numbers
 
 
 def main():
-
-    print("End of main!")
+    # print(generate_binary_numbers(24))
+    N = 24
+    generate_binary_numbers(N)
+    print("\nEnd of main!")
 
 
 if __name__ == '__main__':
